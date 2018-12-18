baaa7c596045becdd1c36a8c49331ff081bf3bee
Abdinaim <45972040+Abdinaim@users.noreply.github.com>
2018-12-18 15:38:49 +0100

    Update bifurcation_diagram.m

--- bifurcation_diagram.m
+++ bifurcation_diagram.m
@@ -1,17 +1,17 @@
-% this program is used to plot the bifurcation diagram. Here we find the
-% fixed points of the system and calculate the max and min voltage values
-% at each time instant starting from a given point. The plot of the voltage
-% values represents the bifurcation diagram.
+% Denne kode bruges til at plotte forskydningsdiagrammet for fitzhugh nagumo modellen. 
+% Her kan fikspunkterne for systemet findes,
+% og disse kan bruges til at udregne max og minimumvÃ¦rdierne for hver tidsenhed til et givent punkt.
+% Plottet reprÃ¦senterer det todelte diagram.
 
 function bifurcation_diagram()
     global a b tau I;
-    % Værdierne af parametrene a, b, tau kan ændres her
+    % VÃ¦rdierne af parametrene a, b, tau kan Ã¦ndres her
     a = 0.7; b = 0.8; tau = 13;
-    % indlæser variable 
+    % indlÃ¦ser variable 
     i = 1;  
     V = zeros; Vmin = zeros; Vmax = zeros;
     
-    % Gå igennem forskellige værdier af i for at se på dens indflydelse som
+    % GÃ¥ igennem forskellige vÃ¦rdier af i for at se pÃ¥ dens indflydelse som
     % forskydningsparameter
     for I=[-1:0.01:1.8]
         % definerer ligningssystemet
@@ -19,13 +19,13 @@ function bifurcation_diagram()
         g = @(y) f(0,y);
         % Find fikspunktet
         fp = fsolve(g,[0 0]);       
-        %Find værdierne af w og V for fikspunktet
+        %Find vÃ¦rdierne af w og V for fikspunktet
         v_fp = fp(1); w_fp = fp(2);   
         tspan = [0 80];
-        % Løser ligningssustemet
+        % LÃ¸ser ligningssustemet
         [tspan u] = ode45(f, tspan , [v_fp+0.2, w_fp]);
         V(i) = v_fp;
-        % Finder minimum og maksimumværdierne for hvert ''step''
+        % Finder minimum og maksimumvÃ¦rdierne for hvert ''step''
         Vmin(i) = min(u(:,1));
         Vmax(i) = max(u(:,1));
         i = i + 1;
